248e3f27b0ad8befdbd68f46d92824e69ce5bebb
oleksandr_shynkariuk <6087491@gmail.com>
2013-08-29 13:53:14 +0200

    first commit

---
+++ html/js/menucontents.js
@@ -0,0 +1,16 @@
+var anylinkmenu1={divclass:'anylinkmenu', inlinestyle:'', linktarget:''} //First menu variable. Make sure "anylinkmenu1" is a unique name!
+anylinkmenu1.items=[
+	
+	["Pricing structure", "price_schedule.html"],
+	["Price of single services", "single-service.html"] //no comma following last entry!
+]
+
+var anylinkmenu2={divclass:'anylinkmenu', inlinestyle:'', linktarget:''} //First menu variable. Make sure "anylinkmenu1" is a unique name!
+anylinkmenu2.items=[
+	
+	
+	["4d Training", "4dtraining.html"] //no comma following last entry!
+]
+
+
+
