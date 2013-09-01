dfb1087d817a54b71bccce727cc2f90374b86627
oleksandr_shynkariuk <6087491@gmail.com>
2013-09-01 19:34:31 +0200

    Fixed 'Om oss' section

---
+++ html/about-us.php
@@ -0,0 +1,5 @@
+<?php
+include_once 'header.html';
+include_once 'about-us.html';
+include_once 'footer.html';
+?>
---
+++ html/index.php
@@ -0,0 +1,5 @@
+<?php
+include_once 'header.html';
+include_once 'index_page.html';
+include_once 'footer.html';
+?>
