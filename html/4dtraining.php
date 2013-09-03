74cb482734070c99c9ed502670653ddd08e4d538
oleksandr_shynkariuk <6087491@gmail.com>
2013-09-03 19:26:40 +0200

    Fixed several sections, added Lars

--- html/4dtraining.php
+++ html/4dtraining.php
@@ -1,6 +1,5 @@
 <?php
-$page="company_health";
 include_once 'header.html';
-include_once 'company_health.html';
+include_once '4dtraining.html';
 include_once 'footer.html';
 ?>
--- html/50plus.php
+++ html/50plus.php
@@ -1,5 +1,6 @@
 <?php
 $page = "50plus";
 include_once 'header.html';
+include_once '50plus.html';
 include_once 'footer.html';
 ?>
