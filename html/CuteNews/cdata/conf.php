cfdae40be6571e34e44d54ec3e69a15bd54a3670
oleksandr_shynkariuk <6087491@gmail.com>
2013-09-09 19:22:33 +0200

    Added 'CuteNews' library

---
+++ html/CuteNews/cdata/conf.php
@@ -0,0 +1,2 @@
+<?php die(); ?>
+a:1:{s:10:"crypt_salt";s:128:"e3847e9be5d06f7a1fac62c3da47e342417b969371c67b6a29bede5e07be2c99b41c5f54242b4e5debeb79e3a3919a6228767f759c4d569fe5e5f4759cef0df8";}
---
+++ html/CuteNews/cdata/users.db.php
@@ -0,0 +1,2 @@
+<?php die("You don't have access to open this file!"); ?>
+1378747263|1|lifesymb|5329cc2d610687d2b6798d45d6d394892c1e23ca82f9c19fd575f9900ba395a2||oleksandr.shynkariuk@gmail.com|0|1||1378747286
